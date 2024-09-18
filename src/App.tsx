3e8ce441b2e0173039d004e3f307be77471789d5
Leo Yassuda <leoyassuda@pop-os.localdomain>
2024-09-18 16:53:09 -0300

    feat: 🎸 add expand-card & ajust app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Profile from "./components/Profile";
-import LinkButton from "./components/LinkButton";
-import links from "./data/links";
 import navLinks from "./data/navigations";
 import { FloatingDock } from "./components/ui/floating-dock";
+import { ExpandableCard } from "./components/ExpandCard";
 
 const App: React.FC = () => {
   return (
@@ -15,27 +14,16 @@ const App: React.FC = () => {
           <FloatingDock items={navLinks} />
         </div>
 
-        <div className="p-2">
-          <div>
-            {links.map((link, index) => (
-              // <LinkPreview key={index} url={link.url}>
-              <LinkButton key={index} href={link.url} text={link.text} />
-              // </LinkPreview>
-            ))}
-          </div>
-        </div>
-
-        <div className="p-2 w-full h-80 md:h-96">
+        <div className="p-2 w-full h-80 md:h-96 aspect-w-16 aspect-h-9">
           <iframe
             width="100%"
             height="100%"
             src="https://www.youtube.com/embed/53O0UbUKHj4?si=H9bUBwDHsTr5TWOk"
             title="Maniac - Stray Kids - Dance Cover"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-            className="rounded-3xl"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="rounded-3xl border-spacing-0"
           ></iframe>
         </div>
 
@@ -43,13 +31,12 @@ const App: React.FC = () => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/g00TOlI6eoA?si=nlH9Tm1VWODZnSwj"
+            src="https://www.youtube.com/embed/g00TOlI6eoA?si=Q7nN6GxK-2FV_qSv"
             title="Cry For Me - Twice - Dance Cover"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-            className="rounded-3xl"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="rounded-3xl border-spacing-0"
           ></iframe>
         </div>
 
@@ -59,26 +46,15 @@ const App: React.FC = () => {
             height="100%"
             src="https://www.youtube.com/embed/rI1HC9JfQ_0?si=RnC8cROE5dAI4adP"
             title="Over My Skin - Gabriel Shimoda Coreography"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-            className="rounded-3xl"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="rounded-3xl border-spacing-0"
           ></iframe>
         </div>
 
-        <div className="p-2 w-full h-80 md:h-96">
-          <iframe
-            title="PlayLeoist - Jp. Ver."
-            src="https://open.spotify.com/embed/playlist/0E1ANTyC88J0pG0HSsDEX8?utm_source=generator"
-            width="100%"
-            height="352"
-            frameBorder="0"
-            allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-            className="rounded-3xl"
-          ></iframe>
+        <div className="p-2">
+          <ExpandableCard />
         </div>
       </div>
     </div>
