140c818d624aeae0bc4b22a5d16baa61a0eff500
Leo Yassuda <leoyassuda@pop-os.localdomain>
2024-09-18 10:58:15 -0300

    feat: 🎸 add more TY links

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,26 @@ import Profile from "./components/Profile";
 import LinkButton from "./components/LinkButton";
 import links from "./data/links";
 import navLinks from "./data/navigations";
-import { LinkPreview } from "./components/ui/link-preview";
 import { FloatingDock } from "./components/ui/floating-dock";
-import {
-  IconBrandGithub,
-  IconBrandX,
-  IconExchange,
-  IconHome,
-  IconNewSection,
-  IconTerminal2,
-} from "@tabler/icons-react";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex justify-stretch flex-col py-2 sm:py-12">
-      <div className="rounded-3xl grow sm:p-20 ">
+    <div className="min-h-screen flex justify-center justify-items-center items-center flex-col py-2 sm:py-12 ">
+      <div className="md:w-[512px]">
         <Profile />
 
-        <div className="p-2 w-full">
+        <div className="p-2 w-auto">
           <FloatingDock items={navLinks} />
         </div>
 
-        <div className="grid justify-center justify-items-center">
-          <div className="mt-5 space-y-3 ">
+        <div className="p-2">
+          <div>
             {links.map((link, index) => (
+              // <LinkPreview key={index} url={link.url}>
               <LinkButton key={index} href={link.url} text={link.text} />
+              // </LinkPreview>
             ))}
           </div>
-        </div>
-
-        <div className="p-2 w-ful">
-          <LinkPreview url="https://leoyassuda.com">
-            <LinkButton href="https://leoyassuda.com" text="portifolio" />
-          </LinkPreview>
         </div>
 
         <div className="p-2 w-full h-80 md:h-96">
@@ -43,7 +30,7 @@ const App: React.FC = () => {
             width="100%"
             height="100%"
             src="https://www.youtube.com/embed/53O0UbUKHj4?si=H9bUBwDHsTr5TWOk"
-            title="YouTube video player"
+            title="Maniac - Stray Kids - Dance Cover"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
@@ -57,11 +44,39 @@ const App: React.FC = () => {
             width="100%"
             height="100%"
             src="https://www.youtube.com/embed/g00TOlI6eoA?si=nlH9Tm1VWODZnSwj"
-            title="YouTube video player"
+            title="Cry For Me - Twice - Dance Cover"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
+            className="rounded-3xl"
+          ></iframe>
+        </div>
+
+        <div className="p-2 w-full h-80 md:h-96">
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/rI1HC9JfQ_0?si=RnC8cROE5dAI4adP"
+            title="Over My Skin - Gabriel Shimoda Coreography"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            className="rounded-3xl"
+          ></iframe>
+        </div>
+
+        <div className="p-2 w-full h-80 md:h-96">
+          <iframe
+            title="PlayLeoist - Jp. Ver."
+            src="https://open.spotify.com/embed/playlist/0E1ANTyC88J0pG0HSsDEX8?utm_source=generator"
+            width="100%"
+            height="352"
+            frameBorder="0"
+            allowfullscreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
             className="rounded-3xl"
           ></iframe>
         </div>
